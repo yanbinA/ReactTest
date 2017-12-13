628fd401ee6d326bab4ba6344a718e499af6f635
yanbinA <yanbin7019@outlook.com>
2017-12-13 18:15:41 +0800

    react基础入门

---
+++ my-app/src/ShoppingList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatApp</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <ShoppingList />,
+    document.getElementById('shopping')
+);
